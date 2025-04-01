ed1ccfffc356b677b8ba89d00214ce1606b9fc6d
thaArcadeGuy <gilbertsilver64@gmail.com>
2025-04-01 23:25:37 +0300

    code

---
+++ factorial.js
@@ -0,0 +1,19 @@
+function factorial(n) {
+  if (n < 0) {
+    throw new Error("Use Positive Number");
+  } else if (typeof n !== "number") {
+    throw new Error("Use Whole Number");
+  } else if (!Number.isInteger(n)) {
+    throw new Error("Use Whole Number");
+  } else if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  let result = 1;
+
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
